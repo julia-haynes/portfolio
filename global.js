b6396cfb7abdb48ae30b120c27ec5296635fbd46
julia-haynes <jshaynes@ucsd.edu>
2025-10-18 13:15:49 -0700

    Lab 3

---
+++ global.js
@@ -0,0 +1,100 @@
+console.log("IT'S ALIVE!")
+
+
+// function $$(selector, context = document) {
+//     return Array.from(context.querySelectorAll(selector));
+// }
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname ===location.pathname 
+// );
+
+// if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+// }
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/' // Local server
+    : '/portfolio/'; // GitHub Pages repo name 
+
+let pages = [
+    {url: '', title: 'Home' },
+    {url: 'projects/', title: 'Projects' },
+    {url: "contact/",  title: "Contact" },
+    {url: "cv/", title: "CV" },
+    {url: "https://github.com/julia-haynes", title: "GitHub" } 
+    // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;   
+}
+    let title = p.title;
+    // next step: create link and add it to nav 
+    let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+
+a.classList.toggle(
+  'current',
+  a.host === location.host && a.pathname === location.pathname,
+);
+
+let isExternal = a.host && a.host !== location.host;
+if (isExternal) {
+  a.target = "_blank";
+}
+
+nav.append(a);
+
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select id="theme-select">
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+let select = document.querySelector('#theme-select');
+
+if ("colorScheme" in localStorage) {
+    let savedScheme = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', savedScheme);
+    select.value = savedScheme;
+}
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed. to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value 
+    localStorage 
+});
+
+
+
+
+
+
+// select.addEventListener('input', (event) => {
+//     let value = event.target.value;
+//     console.log('Theme changed to:', value);
+// });
+
+
