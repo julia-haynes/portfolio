42ece820c552bc8739752b928847587c81e6e394
julia-haynes <jshaynes@ucsd.edu>
2025-10-24 16:50:13 -0700

    fixed img path

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+
+
 import { fetchJSON, renderProjects } from '../global.js'
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -16,3 +18,11 @@ const titleElement = document.querySelector('.projects-title');
 if (titleElement) {
   titleElement.textContent = `Projects (${projects.length})`;
 }
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'  // for local live server
+    : '/portfolio/';
+
+const img = document.createElement('img');
+img.src = BASE_PATH + projects.image;
