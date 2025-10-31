d2a34c59e9a14cba21daa94ad69c4912eca2de85
julia-haynes <jshaynes@ucsd.edu>
2025-10-30 20:37:58 -0700

    changed const projects

--- projects/projects.js
+++ projects/projects.js
@@ -4,10 +4,11 @@ const BASE_PATH =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
     ? '/' // Local server
     : '/portfolio/';
-import { fetchJSON, renderProjects } from '../global.js'
+import { fetchJSON, renderProjects } from '../global.js';
 //const BASE_PATH = '/portfolio/';
 //const projects = await fetchJSON(BASE_PATH + '../lib/projects.json');
-const projects = await fetchJSON(BASE_PATH + '../lib/projects.json');
+//const projects = await fetchJSON('../lib/projects.json');
+const projects = await fetchJSON(BASE_PATH + 'lib/projects.json');
 //const projects = await fetchJSON('lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
