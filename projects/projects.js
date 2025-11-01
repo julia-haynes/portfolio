64e0c193f4acc2726981dbf9085ad3957e5c281e
julia-haynes <jshaynes@ucsd.edu>
2025-10-31 18:16:13 -0700

    up to 5.3, added piechart click (turns pink)

--- projects/projects.js
+++ projects/projects.js
@@ -105,13 +105,22 @@ function renderPieChart(projectsGiven) {
   let colors = d3.scaleOrdinal(d3.schemeTableau10);
   newArcs.forEach((arc, idx) => {d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx))});
   let legend = d3.select('.legend');
-  newData.forEach((d, idx) => {legend.append('li').attr('style', `--color:${colors(idx)}`).html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); }); }
+  newData.forEach((d, idx) => {legend.append('li').attr('style', `--color:${colors(idx)}`).html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); }); 
+  
+  let selectedIndex = -1
+  let svg = d3.select('svg');
+  svg.selectAll('path').remove();
+  newArcs.forEach((arc, i) => {svg.append('path').attr('d', arc).attr('fill', colors(i)).on('click', () => {selectedIndex = selectedIndex === i ? -1 : i; svg.selectAll('path').attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : '')); legend .selectAll('li').attr('class', (_, idx) => (idx === selectedIndex ? 'selected' :''));});});
+  
+}
 
 renderPieChart(projects);
 searchInput.addEventListener('input', (event) =>{query = event.target.value; let filteredProjects = projects.filter((project) =>{let values = Object.values(project).join('\n').toLowerCase();return values.includes(query.toLowerCase());}); renderProjects(filteredProjects, projectsContainer, 'h2');renderPieChart(filteredProjects);});
 
+/*
 let selectedIndex = -1
-selectedIndex === i ? -1 : i;
+//selectedIndex === i ? -1 : i;
 let svg = d3.select('svg');
 svg.selectAll('path').remove();
-arcs.forEach((arc, i) => {svg.append('path').attr('d', arc).attr('fill', colors(i)).on('click', () => {selectedIndex = selectedIndex === i ? -1 : i; svg.selectAll('path').attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : '')); legend .selectAll('li').attr('class', (_, idx) => (idx === selectedIndex ? 'selected' :''));});});
+newArcs.forEach((arc, i) => {svg.append('path').attr('d', arc).attr('fill', colors(i)).on('click', () => {selectedIndex = selectedIndex === i ? -1 : i; svg.selectAll('path').attr('class', (_, idx) => (idx === selectedIndex ? 'selected' : '')); legend .selectAll('li').attr('class', (_, idx) => (idx === selectedIndex ? 'selected' :''));});});
+*/
